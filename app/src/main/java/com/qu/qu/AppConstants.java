6e658b950986e98c7e117d69e0bc04115ac33980
Taylor Sloan <taylor.sloan2@gmail.com>
2016-12-06 22:11:41 -0800

    committing for Github upload

--- app/src/main/java/com/qu/qu/AppConstants.java
+++ app/src/main/java/com/qu/qu/AppConstants.java
@@ -5,6 +5,6 @@ package com.qu.qu;
  */
 public class AppConstants {
 
-    public static final String API_KEY = "1c5ab22b17b6479d311a7687711b6eaa04054b2926b37db1f10076fd";
-    public static final String SERVER_URL = "https://floating-taiga-2201.herokuapp.com";
+    public static final String API_KEY = "test-key";
+    public static final String SERVER_URL = "test-url";
 }
